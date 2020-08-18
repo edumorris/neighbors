06d2098999c6676b44b163d0ca39435eb4301379
alishakavene <akmunywoki@gmail.com>
2020-08-18 17:30:14 +0300

    create class for adim

--- neighbors/admin.py
+++ neighbors/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Neighborhood,Profile,Business,Contact
 
-# Register your models here.
+
+admin.site.register(Neighborhood)
+admin.site.register(Profile)
+admin.site.register(Contact)
+admin.site.register(Business)
