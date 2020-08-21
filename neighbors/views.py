92b44fc4780782ad90024fa1971d3f564f3d7d4d
edumorris <eduuwachira@gmail.com>
2020-08-21 11:59:38 +0300

    Business search base function effected

--- neighbors/views.py
+++ neighbors/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 #APIs
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Neighborhood
+from .models import Neighborhood, Business
 from .serializer import HoodSerializer
 from rest_framework import status
 
@@ -17,6 +17,19 @@ def index_test(request):
     title = "Iper testpage"
 
     return render(request, 'index.html', {"title": title})
+
+# Business Search
+def business_search(request):
+    title = "Search Results"
+    
+    if 'business' in request.GET and request.GET["business"]:
+        search_term = request.GET.get("business")
+        searched_business = Business.objects.filter(business_name__icontains=search_term)
+
+        return render(request, 'business_search.html', {"title": title, "businesses": searched_business})
+    else:
+        message = "No search term added"
+        return render(request, 'business_search.html', {"title": title, "message": message})
 
 # API Classes
 class Hoods(APIView):
